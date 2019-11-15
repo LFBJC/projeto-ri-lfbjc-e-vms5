5058f4ac0d6092c8991ece2373ca44165240c883
LFBJC <lfbjc@cin.ufpe.br>
2019-11-15 15:06:12 -0300

    inicio do arquivo de processamento de consultas

---
+++ consultas.py
@@ -0,0 +1,15 @@
+"""
+TODO
+Criar documento de indice invertido
+Concluir este código
+"""
+query = raw_input('digite aqui os termos da consulta: ')
+tipo_de_busca = raw_input('digite \'a\' para busca avançada ou \'c\' para busca comum:')
+qtos_de = input('digite o número mínimo de quartos: ')
+qtos_ate = input('digite o número máximo de quartos: ')
+banheiros_de = input('digite o número mínimo de banheiros: ')
+banheiros_ate = input('digite o número máximo de banheiros: ')
+area_de = input('digite a área mínima: ')
+area_ate = input('digite a área máxima: ')
+preco_de = input('digite o preço mínimo: ')
+preco_ate = input('digite o preço máximo: ')

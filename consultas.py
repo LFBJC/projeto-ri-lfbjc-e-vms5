b1d69767c095b87b06108f38e19bc5a6b3e7b5f6
LFBJC <lfbjc@cin.ufpe.br>
2019-11-15 15:06:12 -0300

    inicio do arquivo de processamento de consultas

---
+++ consultas.py
@@ -0,0 +1,5 @@
+
+query = raw_input('digite aqui os termos da consulta: ')
+qtos = input('digite o número mínimo de quartos: ')
+banheiros = input('digite o número mínimo de banheiros: ')
+area = input('digite a área mínima: ')

99a484141b1c1626d0c4696f7c5cee1e0a79f391
Luiz Felipe de Barros Jordao Costa <lfbjc@ug4c14.windows.cin.ufpe.br>
2019-11-19 12:37:40 -0300

    atualização do tratamento de consultas

--- consultas.py
+++ consultas.py
@@ -3,7 +3,6 @@ TODO
 rankear usando modelo de espaço de vetores com e sem tf idf
 calc correlação dos ranks
 """
-import pandas as pd
 import json
 query = raw_input('digite aqui os termos da consulta: ')
 query_terms = query.split()
@@ -12,19 +11,17 @@ with open('index.json') as json_file:
 docs = []
 for t in query_terms:
 	docs.append(data[t])
+with open('ordem de documentos.txt') as docs_file:
+    files = docs_file.read().replace('\n', '').replace(' ','').replace('[', '').replace(']','').split(',')
 filenames = []
-file_data_base = pd.read_csv('Rotulos_sites.csv')
-urls = []
 for d in docs:
-	urls.append(file_data_base.iloc[1,d[0]])
+	filenames.append(files[d[0]])
 #tratamento da busca avançada
 tipo_de_busca = raw_input('digite \'a\' para busca avançada ou \'c\' para busca comum: ')
 qtos_de = 0
 qtos_ate = 4294967296
 banheiros_de = 0
 banheiros_ate = 4294967296
-area_de = 0
-area_ate = 4294967296
 preco_de = 0
 preco_ate = 4294967296
 if (tipo_de_busca == 'a'):
@@ -32,7 +29,14 @@ if (tipo_de_busca == 'a'):
 	qtos_ate = input('digite o número máximo de quartos: ')
 	banheiros_de = input('digite o número mínimo de banheiros: ')
 	banheiros_ate = input('digite o número máximo de banheiros: ')
-	area_de = input('digite a área mínima: ')
-	area_ate = input('digite a área máxima: ')
 	preco_de = input('digite o preço mínimo: ')
 	preco_ate = input('digite o preço máximo: ')
+elements =[]
+for name in filenames:
+	with open('.\\info_pages\\'+name+'.json') as element:
+		elem_features = json.load(element);
+		if((elem_features['Quartos']>qtos_de)&(elem_features['Quartos']<qtos_ate)&(elem_features['Banheiros']>banheiros_de)&(elem_features['Banheiros']<banheiros_ate)&(elem_features['Preco']>preco_de)&(elem_features['Preco']<preco_ate)):
+			elements.append(elem_features)
+#rankeamento
+
+#exibição da busca

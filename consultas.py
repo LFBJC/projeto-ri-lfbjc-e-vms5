b73b25d273d31552e07e2a87bed68de0502c0c56
LFBJC <lfbjc@cin.ufpe.br>
2019-11-21 17:15:06 -0300

    pronto para pesquisa simples

--- consultas.py
+++ consultas.py
@@ -6,8 +6,8 @@ calc correlação dos ranks
 """
 import os
 import json
-import vector_space_model as vectspmod
-print(os.getcwd())
+#import vector_space_model as vectspmod
+
 query = raw_input('digite aqui os termos da consulta: ')
 query_terms = query.split()
 with open('indexcomprimido.json') as json_file:
@@ -21,8 +21,6 @@ with open('ordem de documentos.txt') as docs_file:
 filenames = []
 for d in docs:
 	d_ = [i[0] for i in d]
-	print(d_)
-	print(files)
 	filenames.extend([files[i] for i in d_])
 #tratamento da busca avançada
 tipo_de_busca = raw_input('digite \'a\' para busca avançada ou \'c\' para busca comum: ')
@@ -40,14 +38,21 @@ if (tipo_de_busca == 'a'):
 	preco_de = input('digite o preço mínimo: ')
 	preco_ate = input('digite o preço máximo: ')
 elements =dict()
+content_dict = dict()
 for name in filenames:
 	name = name.replace('\'','').replace('\r','')
 	with open('info_pages/'+name+'.json') as element_json:
 		with open('wrapper_files/'+name+'.html') as document:
-			elem_features = json.load(element_json);
-			if((elem_features['Quartos']>qtos_de)&(elem_features['Quartos']<qtos_ate)&(elem_features['Banheiros']>banheiros_de)&(elem_features['Banheiros']<banheiros_ate)&(elem_features['Preco']>preco_de)&(elem_features['Preco']<preco_ate)):
-				elements[document]=(elem_features)
+			elem_features = json.load(element_json,encoding='utf-8');
+			#print(elem_features)
+			#if((elem_features['Quartos']>qtos_de)&(elem_features['Quartos']<qtos_ate)&(elem_features['Banheiros']>banheiros_de)&(elem_features['Banheiros']<banheiros_ate)&(elem_features['Preco']>preco_de)&(elem_features['Preco']<preco_ate)):
+			#	print('chegou!')
+			#	elements[name]=elem_features
+			#	content_dict[name] = document.read().replace('\n',' ').replace('\r',' ')
+			#elements[document.read().replace('\n',' ').replace('\r',' ')]=(elem_features)
 #rankeamento
-vsm = vectspmod.vector_space_model(elements.keys())
-rank1 = vsm.search([query])
+#vsm = vectspmod.vector_space_model()
+#rank1 = vsm.rank(query,content_dict)
 #exibição da busca
+for v in elements.values():
+	print(v)
--- vector_space_model.py
+++ vector_space_model.py
@@ -1,56 +1,65 @@
 # -*- coding: utf-8 -*-
 import nltk
-from selectolax.parser import HTMLParser
-#fazer inicializacao d documentVectors
-class vector_space_model:
-    def __init__(self,documentList):
-        self.parser = HTMLParser
-        self.stemmer = PorterStemmer()
-        self.stopwords = nltk.corpus.stopwords.words('portuguese')
-        self.documentVectors = self.makeVector(self.getVectorKeywordIndex(documentList))
-    def removeStopWords(self,list):
-                 """ Remove common words which have no search value """
-                 return [word for word in list if word not in self.stopwords ]
-    def tokenise(self, string):
-                 """ break string up into tokens and stem words """
-                 string = self.clean(string)
-                 words = string.split(" ")
-                 return [self.stemmer.stem(word,0,len(word)-1) for word in words]
-    def getVectorKeywordIndex(self, documentList):
-            """ create the keyword associated to the position of the elements within the document vectors """
-    
-            #Mapped documents into a single word string
-            vocabularyString = " ".join(documentList)
+from bs4 import BeautifulSoup
+import collections
+import re
+from sklearn.feature_extraction.text import CountVectorizer
 
-            vocabularyList = self.parser.tokenise(vocabularyString)
-            #Remove common words which have no search value
-            vocabularyList = self.parser.removeStopWords(vocabularyList)
-            uniqueVocabularyList = util.removeDuplicates(vocabularyList)
+class vector_space_model:
+    def __init__(self):
+        self.stemmer = nltk.stem.PorterStemmer()
+        self.stopwords = nltk.corpus.stopwords.words('portuguese')
     
-            vectorIndex={}
-            offset=0
-            #Associate a position with the keywords which maps to the dimension on the vector used to represent this word
-            for word in uniqueVocabularyList:
-                    vectorIndex[word]=offset
-                    offset+=1
-            return vectorIndex  #(keyword:position)
-    def makeVector(self, wordString):
+    def word_extraction(self, sentence):
+        stopwords = nltk.corpus.stopwords.words('portuguese')
+        #words = re.sub("[^\w]", " ", sentence).split()
+        #words = word_tokenize(sentence, 'portuguese')
+        words = re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', sentence).split()
+        #words = words.split('[A-Z][^A-Z]*')
+        cleaned_text = [w.lower() for w in words if w not in stopwords]
+        return cleaned_text
+    def get_vectors(self, dictionary):
             """ @pre: unique(vectorIndex) """
-    
-            #Initialise vector with 0's
-            vector = [0] * len(self.vectorKeywordIndex)
-            wordList = self.parser.tokenise(wordString)
-            wordList = self.parser.removeStopWords(wordList)
-            for word in wordList:
-               vector[self.vectorKeywordIndex[word]] += 1; #Use simple Term Count Model
-            return vector
+            vectorizer = CountVectorizer(stop_words=nltk.corpus.stopwords.words('portuguese'))
+            Z = vectorizer.fit_transform(dictionary.values())
+            vocab = vectorizer.get_feature_names()
+            vectorizer.fit_transform(allsentences)
+            print(vocab)
+            vectors = dict()
+            for (k,sentence) in dictionary.items():
+                words = self.word_extraction(sentence)
+                vectors[k] = [0] * len(vocab)
+            for w in words:
+                for i, word in enumerate(vocab):
+                   if word == w:
+                            vectors[k][i] += 1
+            return vectors
     def cosine(vector1, vector2):
             """ related documents j and q are in the concept space by comparing the vectors :
                     cosine  = ( V1 * V2 ) / ||V1|| x ||V2|| """
             return float(dot(vector1,vector2) / (norm(vector1) * norm(vector2)))
-    def search(self,searchList):
+    def rank(self,query,dictionary):
         """ search for documents that match based on a list of terms """
-        queryVector = self.buildQueryVector(searchList)
-        ratings = [util.cosine(queryVector, documentVector) for documentVector in self.documentVectors]
-        ratings.sort(reverse=True)
+        dictionary['query']=query
+        vectors = self.get_vectors(dictionary)
+        queryVector = vectors['query']
+        del vectors['query']
+        ratings = {{k: self.cossine(v,queryVector) for k, v in vectors.items()}}
+        ratings = colletions.OrderedDict(sorted(ratings.items(), key=operator.itemgetter(1),reverse=True))
         return ratings
+    def rank_tf_idf(self,query,dictionary):
+        """ search for documents that match based on a list of terms """
+        dictionary['query']=query
+        vectors = self.get_vectors(dictionary)
+        queryVector = vectors['query']
+        del vectors['query']
+        vectors = tf_idf_vectors(vectors)
+        ratings = {{k: self.cossine(v,queryVector) for k, v in vectors.items()}}
+        ratings = colletions.OrderedDict(sorted(ratings.items(), key=operator.itemgetter(1),reverse=True))
+        return ratings
+    def tf_idf_vectors(vectors,queryVector):
+        idf = create_idf_vector(vectors)
+        vectors = {{k: [t*idf for t in v] for k, v in vectors.items()}}
+        return vectors
+    def create_idf_vector(vectors):
+        
